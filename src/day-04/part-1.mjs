9b92359da9e1096f28f94b113aea21d22c63faad
karlthomas3 <114147680+karlthomas3@users.noreply.github.com>
2023-12-04 21:58:24 +0000

    day 4. recursion breaks my brain

--- src/day-04/part-1.mjs
+++ src/day-04/part-1.mjs
@@ -1,3 +1,29 @@
 export async function processPart1(input) {
-  return "hello";
+  const lines = input.split("\n");
+  const cards = lines.map((line) => {
+    const [card, list] = line.split(": ");
+    if (!list) return;
+    const parts = list.split("| ");
+
+    const id = card.replace("Card ", "");
+    const winners = parts[0].split(" ");
+    const scratches = parts[1].split(" ");
+
+    let points = 0;
+    let multiplier = 1;
+    scratches.forEach((entry) => {
+      if (entry == "") return;
+      if (winners.includes(entry)) {
+        points = multiplier;
+        multiplier *= 2;
+      }
+    });
+    return isFinite(points) ? points : 0;
+  });
+  // console.log(cards);
+  let total = 0;
+  cards.forEach((point) => {
+    if (isFinite(point)) total += point;
+  });
+  return total;
 }
--- src/day-04/part-2.mjs
+++ src/day-04/part-2.mjs
@@ -1,3 +1,45 @@
 export async function processPart2(input) {
-  return "world";
+  const lines = input.split("\n");
+  const cards = lines.map((line) => {
+    const [card, list] = line.split(": ");
+    if (!list) return;
+    const parts = list.split("| ");
+
+    const id = parseInt(card.replace("Card ", ""));
+    const winners = parts[0].split(" ");
+    const scratches = parts[1].split(" ");
+
+    let points = 0;
+    scratches.forEach((entry) => {
+      if (entry == "") return;
+      if (winners.includes(entry)) points++;
+    });
+    if (isFinite(id) && isFinite(points)) {
+      return { id, points };
+    } else return "wtf";
+  });
+
+  return score(cards);
+}
+
+function score(cards) {
+  let instances = [];
+
+  function process(index, copies) {
+    if (index >= cards.length || copies === 0) return;
+
+    let card = cards[index];
+    if (!card) return;
+
+    instances[card.id] = (instances[card.id] || 0) + copies;
+    for (let i = 1; i <= card.points; i++) {
+      process(index + i, copies);
+    }
+  }
+
+  cards.forEach((card, index) => {
+    if (card) process(index, 1);
+  });
+
+  return instances.reduce((acc, count) => acc + (count || 0), 0);
 }
