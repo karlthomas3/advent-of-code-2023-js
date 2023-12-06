09738c30655540f6ecd94e2b754ec3a222926ffd
karlthomas3 <114147680+karlthomas3@users.noreply.github.com>
2023-12-06 14:34:59 +0000

    day 6 complete

--- src/day-06/part-1.mjs
+++ src/day-06/part-1.mjs
@@ -1,3 +1,29 @@
 export async function processPart1(input) {
-  return "hello";
+  const lines = input.split("\n");
+  const numbers = lines.map((line) => {
+    const matches = line.match(/\d+/g);
+    return matches ? matches.map(Number) : [];
+  });
+
+  const races = numbers[0].map((number, index) => {
+    const raceTime = number;
+    const recordDistance = numbers[1][index];
+    return { raceTime, recordDistance };
+  });
+
+  function countWins(race) {
+    const { raceTime, recordDistance } = race;
+
+    let wins = [];
+    for (let i = 0; i < raceTime; i++) {
+      const run = raceTime - i;
+      const distance = run * i;
+      if (distance > recordDistance) wins.push(i);
+      // console.log("hold:", i, "run:", run, "distance:", distance);
+    }
+    return wins.length;
+  }
+  const winArr = races.map((race) => countWins(race));
+  const marginOfError = winArr.reduce((a, b) => a * b, 1);
+  return marginOfError;
 }
--- src/day-06/part-2.mjs
+++ src/day-06/part-2.mjs
@@ -1,3 +1,26 @@
 export async function processPart2(input) {
-  return "world";
+  const lines = input.split("\n");
+  const data = lines.map((line) => {
+    if (!line) return;
+    return line.split(":")[1].replaceAll(" ", "");
+  });
+
+  const race = {
+    raceTime: Number(data[0]),
+    recordDistance: Number(data[1]),
+  };
+
+  function countWins(race) {
+    const { raceTime, recordDistance } = race;
+
+    let wins = [];
+    for (let i = 0; i < raceTime; i++) {
+      const run = raceTime - i;
+      const distance = run * i;
+      if (distance > recordDistance) wins.push(i);
+      // console.log("hold:", i, "run:", run, "distance:", distance);
+    }
+    return wins.length;
+  }
+  return countWins(race);
 }
